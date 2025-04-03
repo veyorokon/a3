8cf220362a82453ec5b987b3183686fa821e7f7b
费政聪 <3300949577@qq.com>
2025-04-03 18:36:29 +0800

    background crop

--- models/utils.py
+++ models/utils.py
@@ -20,6 +20,23 @@ def _crop_and_resize_pad(image, height=480, width=720):
     return image 
 
 
+def _crop_and_resize(image, height=512, width=512):
+    image = np.array(image)
+    image_height, image_width, _ = image.shape
+    if image_height / image_width < height / width:
+        croped_width = int(image_height / height * width)
+        left = (image_width - croped_width) // 2
+        image = image[:, left: left+croped_width]
+        image = Image.fromarray(image).resize((width, height))
+    else:
+        croped_height = int(image_width/width*height)
+        top = (image_height - croped_height) // 2
+        image = image[top:top+croped_height, :]
+        image = Image.fromarray(image).resize((width, height))
+
+    return image
+    
+
 def write_mp4(video_path, samples, fps=14, audio_bitrate="192k"):
     clip = ImageSequenceClip(samples, fps=fps)
     clip.write_videofile(video_path, audio_codec="aac", audio_bitrate=audio_bitrate, 
